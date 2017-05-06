980e7096749e8c27326e4875bc8bf7dd1cc9ec5b
AliaaAbdElhalim <aliaaabdelhalim434@gmail.com>
2017-05-06 17:06:16 +0200

    modify validation

--- frontend/chats/www/js/controller/login.js
+++ frontend/chats/www/js/controller/login.js
@@ -1,10 +1,11 @@
 angular.module("chat app").controller('login',function($scope,User,$state){
 
-$scope.user={}
-
+$scope.login={}
+if(valid){
 $scope.login = function(){
-    localStorage.setItem("username", $scope.user.username);
+    localStorage.setItem("username", $scope.login.username);
     $state.go('base.active');
+}
 }
 
 })
