ae5fbbe5e599c023fd89a72e959b00dfe1bc8eaf
AliaaAbdElhalim <aliaaabdelhalim434@gmail.com>
2017-05-06 14:50:09 +0200

    create routes file

---
+++ frontend/chats/www/js/routes.js
@@ -0,0 +1,61 @@
+angular.module('osApp').config(function($stateProvider) {
+  $stateProvider
+  
+  state('home',{
+    url:'/',
+    templateUrl:"templates/home2.html",
+   
+  })
+
+  .state('login',{
+    url:'/login',
+    templateUrl:"templates/login.html",
+     controller: "login"
+   
+  })
+
+    .state('signup',{
+    url:'/signup',
+    templateUrl:"templates/signup.html",
+    controller: "signup"
+   
+  })
+
+
+    .state('base',
+    templateUrl:"templates/base.html",
+    abstract:true,
+    controller: "base"
+    })
+
+
+    .state('base.about',{
+    url:'/about',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/about.html"
+      }
+    }
+  })
+
+     .state('base.chat',{
+    url:'/chat',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/chat.html"
+      }
+    }
+  })
+
+
+      .state('base.active',{
+    url:'/active',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/active.html"
+      }
+    }
+  })
+
+
+})
