efd44c8b0cedbc9ff00952c76efc20647a8dc047
maiosman <maiosman016@gmail.com>
2017-05-06 17:47:37 +0200

    edit login controller

--- frontend/chats/www/js/controller/login.js
+++ frontend/chats/www/js/controller/login.js
@@ -3,6 +3,7 @@ angular.module("chat app").controller('login',function($scope,User,$state){
 $scope.user={}
 
 $scope.login = function(){
+  
     localStorage.setItem("username", $scope.user.username);
     $state.go('base.active');
 }
