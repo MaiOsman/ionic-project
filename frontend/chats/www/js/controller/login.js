414b8656e2bb855cade7db396cf03792ca2683b5
sara <sarasherif30@gmail>
2017-05-07 17:32:34 +0200

    change in backend and frontend

--- frontend/chats/www/js/controller/login.js
+++ frontend/chats/www/js/controller/login.js
@@ -5,16 +5,19 @@ $scope.login={}
 $scope.login = function(valid){
 if(valid){
   console.log($scope.login.username);
-
     // localStorage.setItem("username", $scope.login.username);
     // $state.go('app.active');
+    // console.log($scope.login.username+" and " + $scope.login.password);
     User.login($scope.login.username,$scope.login.password).then(function(data){
+      // console.log("first"+data);
         if(data == 1){
+          // console.log("before"+data);
             localStorage.setItem("username", $scope.login.username);
             // socket.emit('join', $scope.login.username);
             $state.go('app.active');
         }else{
             console.log("notvalid");
+            // console.log("after"+data);
         }
     })
   }
